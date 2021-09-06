c89cdf81799df801d06d9d9e07d7ef8e2f3b5d2f
+ <+>
2021-09-05 22:51:57 -0500

    commit tree b55bb5f7a0fbd5a3337e9b456ddda8d35097ab73

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (){
+    changeText('This is really cool!')
+});
+
+
+function changeText(text) {
+    let p = document.querySelector('#text');
+    p.textContent = text;
+}
